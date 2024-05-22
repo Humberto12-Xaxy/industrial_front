e9467d722f72f09e1a46947bbb3364bb6ce69c7a
Humberto12-Xaxy <humbertosuriano24@gmail.com>
2024-05-21 23:32:43 -0600

    add url deploying

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const url = 'http://127.0.0.1:8000';
+const url = 'https://industrial-pvbr.onrender.com';
 
 export const getProcesses = async () => {
     return await axios.get(`${url}/`);
